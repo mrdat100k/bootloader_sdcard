94944c1003ff45c8c1de014d900faa86b4ff843e
Nguyễn Bá Đạt <32257724+mrdat100k@users.noreply.github.com>
2017-11-17 17:20:51 +0700

    test bootloader

---
+++ src/main.cpp
@@ -0,0 +1,95 @@
+#include "mbed.h"
+#include "rtos.h"
+#include "SDBlockDevice.h"
+#include "FATFileSystem.h"
+
+#define SD_MOUNT_PATH           "sd"
+#define FULL_UPDATE_FILE_PATH   "/sd/firmware.bin"
+#define POST_APPLICATION_ADDR 0x08015000
+//Pin order: MOSI, MISO, SCK, CS
+SDBlockDevice sd(PA_7, PA_6,
+                 PA_5, PA_4);
+FATFileSystem fs(SD_MOUNT_PATH);
+FlashIAP flash;
+
+void apply_update(FILE *file, uint32_t address);
+
+int main()
+{
+    sd.init();
+    fs.mount(&sd);
+
+    FILE *file = fopen("/sd/firmware.bin", "rb");
+    if (file != NULL) {
+        printf("Firmware update found\r\n");
+
+        apply_update(file, POST_APPLICATION_ADDR);
+
+        fclose(file);
+      //  remove("/sd/firmware.bin");
+    } else {
+        printf("No update found to apply\r\n");
+    }
+
+    fs.unmount();
+    sd.deinit();
+
+    printf("Starting application\r\n");
+
+    mbed_start_application(POST_APPLICATION_ADDR);
+}
+
+void apply_update(FILE *file, uint32_t address)
+{
+    fseek(file, 0, SEEK_END);
+    long len = ftell(file);
+    printf("Firmware size is %ld bytes\r\n", len);
+    fseek(file, 0, SEEK_SET);
+
+    flash.init();
+
+    const uint32_t page_size = flash.get_page_size();
+    char *page_buffer = new char[page_size];
+    uint32_t addr = address;
+    uint32_t next_sector = addr + flash.get_sector_size(addr);
+    bool sector_erased = false;
+    size_t pages_flashed = 0;
+    uint32_t percent_done = 0;
+    while (true) {
+
+        // Read data for this page
+        memset(page_buffer, 0, sizeof(page_buffer));
+        int size_read = fread(page_buffer, 1, page_size, file);
+        if (size_read <= 0) {
+            break;
+        }
+
+        // Erase this page if it hasn't been erased
+        if (!sector_erased) {
+            flash.erase(addr, flash.get_sector_size(addr));
+            sector_erased = true;
+        }
+
+        // Program page
+        flash.program(page_buffer, addr, page_size);
+
+        addr += page_size;
+        if (addr >= next_sector) {
+            next_sector = addr + flash.get_sector_size(addr);
+            sector_erased = false;
+        }
+
+        if (++pages_flashed % 3 == 0) {
+            uint32_t percent_done_new = ftell(file) * 100 / len;
+            if (percent_done != percent_done_new) {
+                percent_done = percent_done_new;
+                printf("Flashed %3ld%%\r", percent_done);
+            }
+        }
+    }
+    printf("Flashed 100%%\r\n", ftell(file), len);
+
+    delete[] page_buffer;
+
+    flash.deinit();
+}
